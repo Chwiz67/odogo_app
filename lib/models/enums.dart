2a3ce90de167be24396ce87e8e4e3325d8a0b582
Inesh1612 <ineshaggarwal24@gmail.com>
2026-03-20 11:29:42 +0530

    WARNING::::: DONT TOUCH THIS PUSH

--- lib/models/enums.dart
+++
@@ -1,4 +0,0 @@
-enum UserRole { commuter, driver }
-enum DriverMode { online, offline, busy }
-enum TripStatus { pending, confirmed, ongoing, scheduled, completed, cancelled }
-enum NotificationType { tripUpdate, demandAlert, systemAlert }
